715d222a34c16a4a15d71a02a012be9e556668a9
NOGOCUS <grigory201462@gmail.com>
2025-05-01 19:23:03 +0300

    delete added for routes and drivers

--- main.py
+++ main.py
@@ -32,6 +32,15 @@ def read_drivers(db: Session = Depends(get_db)):
     drivers = db.query(models.Driver).all()
     return drivers
 
+@app.delete("/drivers/{driver_id}")
+def delete_driver(driver_id: int, db: Session = Depends(get_db)):
+    driver = db.query(models.Driver).filter(models.Driver.id == driver_id).first()
+    if not driver:
+        raise HTTPException(status_code=404, detail="Водитель не найден")
+    db.delete(driver)
+    db.commit()
+    return {"status": "Успешно удален", "driver_id": driver_id}
+
 
 @app.post("/locations/", response_model=schemas.LocationResponse)
 def create_location(location: schemas.LocationCreate, db: Session = Depends(get_db)):
@@ -75,3 +84,13 @@ def create_route(route: schemas.RouteCreate, db: Session = Depends(get_db)):
 def read_routes(db: Session = Depends(get_db)):
     routes = db.query(models.Route).all()
     return routes
+
+
+@app.delete("/routes/{route_id}")
+def delete_route(route_id: int, db: Session = Depends(get_db)):
+    route = db.query(models.Route).filter(models.Route.id == route_id).first()
+    if not route:
+        raise HTTPException(status_code=404, detail="Маршрут не найден")
+    db.delete(route)
+    db.commit()
+    return {"status": "Успешно удален", "route_id": route_id}
